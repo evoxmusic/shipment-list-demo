af81a7dab2989eef2fda3623bf2771d507c617ab
Romaric Philogène <rphilogene@qovery.com>
2023-05-26 20:06:25 +0000

    fix backend cors

--- src/main/java/dev/ancaghenade/shipmentlistdemo/config/CorsConfig.java
+++ src/main/java/dev/ancaghenade/shipmentlistdemo/config/CorsConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowedMethods("*");
     }
 }
