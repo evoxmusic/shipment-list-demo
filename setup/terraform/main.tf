3210dd3d9a4ed07330b2b4eb998c97d23c37b7c6
Nancy Chauhan <nancychn1@gmail.com>
2023-07-12 18:14:38 +0530

    Update main.tf

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -10,7 +10,7 @@ provider "aws" {
   region = "eu-central-1"
   access_key                  = "test"
   secret_key                  = "test"
-  s3_use_path_style           = false
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
